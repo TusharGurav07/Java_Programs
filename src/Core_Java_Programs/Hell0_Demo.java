6919adc5b5db7944065bc6c3f61af4a2ebb2ccbd
Tushar Gurav <guravtushar7412@gmail.com>
2026-02-11 00:06:14 +0530

    Hello

---
+++ src/Core_Java_Programs/Hell0_Demo.java
@@ -0,0 +1,11 @@
+package Core_Java_Programs;
+
+public class Hell0_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		System.out.println("Hello Java");
+
+	}
+
+}
